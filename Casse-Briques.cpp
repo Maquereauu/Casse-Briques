73e20ee087964b3684e63ff4d1c883a40b369ae0
Maquereau2 <macrorush@gmail.com>
2023-11-03 21:50:01 +0100

    update : 😈

--- Casse-Briques.cpp
+++ Casse-Briques.cpp
@@ -1,32 +1,47 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 using namespace sf;
 using namespace std;
 
 int main()
 {
-    // create the window
-    RenderWindow window(VideoMode(800, 600), "My window");
-
-    // run the program as long as the window is open
+    float x = 50;
+    float y = 50;
+    RenderWindow window(VideoMode(1920, 1080), "SFML works!");
+    //CircleShape shape(50.f);
+    //shape.setFillColor(Color::Green);
+    RectangleShape rectangle(Vector2f(100.0f, 100.0f));
+    RectangleShape rectangle2(Vector2f(100.0f, 100.0f));
+    rectangle.setFillColor(Color::Green);
+    rectangle2.setFillColor(Color::Blue);
+    rectangle.setPosition(x, y);
+    rectangle2.setPosition(x+100.0f, y+100.0f);
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
         Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
             if (event.type == Event::Closed)
                 window.close();
         }
-
-        // clear the window with black color
-        window.clear(Color::Black);
-
-        // draw everything here...
-        // window.draw(...);
-
-        // end the current frame
+        if (x <= 1820)
+        {
+            x++;
+        }
+        if (y <= 980)
+        {
+            y++;
+        }
+        rectangle.setPosition(x, y);
+        if (x == 1821 && y == 981)
+        {
+            x = 50;
+            y = 50;
+            rectangle.setPosition(50, 50);
+        }
+        window.clear();
+        window.draw(rectangle);
+        window.draw(rectangle2);
         window.display();
     }
 
