612e6153c35d3b12fb0fda56b55bfaec1f7823ca
keltulsan <ebeaufils@gaming.tech>
2023-11-07 14:47:45 +0100

    init vector2

--- Math.cpp
+++ Math.cpp
@@ -6,3 +6,35 @@ std::vector<float> Math::normalizeVector(std::vector<float> vector)
 	normal = pow(normal, 1 / 2.f);
 	return { vector[0] / normal, vector[1] / normal };
 }
+
+Math::Vector2::Vector2(float x, float  y)
+{
+	_x = x;
+	_y = y;
+};
+
+float Math::Vector2::getX()
+{
+	return _x;
+}
+
+float Math::Vector2::getY()
+{
+	return _y;
+}
+
+void Math::Vector2::setX(float x)
+{
+	_x = x;
+}
+
+void Math::Vector2::setY(float y)
+{
+	_y = y;
+}
+
+void Math::Vector2::setAll(float x, float y)
+{
+	_x = x;
+	_y = y;
+}
--- Math.h
+++ Math.h
@@ -4,5 +4,21 @@
 namespace Math
 {
 	std::vector<float> normalizeVector(std::vector<float> vector);
+
+	class Vector2
+	{
+	private:
+		float _x; 
+		float _y;
+
+	public:
+		Vector2(float x, float y);
+
+		float getX();
+		float getY();
+		void setX(float x);
+		void setY(float y);
+		void setAll(float x, float y);
+	};
 };
 
