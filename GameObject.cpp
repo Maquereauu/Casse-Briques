923aed918633d463441659d43c3c781ac75fb6c1
Maquereau2 <macrorush@gmail.com>
2023-11-08 19:18:22 +0100

    update : 🤤

--- GameObject.cpp
+++ GameObject.cpp
@@ -6,11 +6,11 @@ GameObject::GameObject(float sizeX, float sizeY, float posX, float posY, float s
 	_posX = posX;
 	_posY = posY;
 	_speed = speed;
-	_vector = new Math::Vector2(0.f,-1.f);
+	_vector = new Math::Vector2(0.f, -1.f);
 	sf::RectangleShape* rectangle = new sf::RectangleShape(sf::Vector2f(_sizeX, _sizeY));
 	rectangle->setFillColor(sf::Color::Blue);
 	rectangle->setPosition(_posX, _posY);
-	
+
 	_graphic = rectangle;
 
 }
@@ -29,12 +29,12 @@ GameObject::GameObject(float radius, float posX, float posY, float speed)
 	_graphic = circle;
 }
 
-GameObject::GameObject() : GameObject(100.f, 100.f, 100.f, 100.f, 90.f) 
+GameObject::GameObject() : GameObject(100.f, 100.f, 100.f, 100.f, 90.f)
 {
 
 }
 
-sf::Shape& GameObject::getShape() 
+sf::Shape& GameObject::getShape()
 {
 	return *_graphic;
 }
@@ -78,18 +78,12 @@ GameObject* GameObject::collide(const std::vector<GameObject*>& list)
 std::string GameObject::checkCollidingSide(const GameObject& object)
 {
 	Math::Vector2 relativePosition(object._posX - _posX, object._posY - _posY);
-
-	float scalarProduct = relativePosition.scalarProduct(*_vector);
-
-	if (scalarProduct > 0) {
-		if (std::abs(relativePosition._x) > std::abs(relativePosition._y)) {
-			return (relativePosition._x > 0) ? "left" : "right";
-		}
-		else {
-			return (relativePosition._y > 0) ? "top" : "bottom";
-		}
+	if (std::abs(relativePosition._x) > std::abs(relativePosition._y)) {
+		return (relativePosition._x > 0) ? "left" : "right";
 	}
-	return "none";
+	else {
+		return (relativePosition._y > 0) ? "top" : "bottom";
+	}
 }
 
 
@@ -97,7 +91,7 @@ void GameObject::moveShape(float deltaTime, Math::Vector2 direction)
 {
 	_posX = _posX + (direction.x * deltaTime) * _speed;
 	_posY = _posY + (direction.y * deltaTime) * _speed;
-	
+
 	_graphic->setPosition(_posX, _posY);
 }
 
@@ -109,12 +103,12 @@ void GameObject::rotateShape(float rotateDegree)
 
 void GameObject::setOriginPoint()
 {
-	_graphic->setOrigin(_sizeX / 2.f , _sizeY / 2.f);
+	_graphic->setOrigin(_sizeX / 2.f, _sizeY / 2.f);
 }
 
 void GameObject::setOriginPointOnBase()
 {
-	_graphic->setOrigin(_sizeX / 2.f,  (3 *_sizeY) / 4);
+	_graphic->setOrigin(_sizeX / 2.f, (3 * _sizeY) / 4);
 }
 Math::Vector2 GameObject::getPos()
 {
