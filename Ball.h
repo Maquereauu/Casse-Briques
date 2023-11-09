ed647b9c78f3c64d1eee6be7eff7305e4130075c
Maquereauu <macrorush@gmail.com>
2023-11-09 10:46:38 +0100

    update : test

--- Ball.h
+++ Ball.h
@@ -2,8 +2,6 @@
 #include "GameObject.h"
 class Ball : public GameObject
 {
-private:
-	Math::Vector2* _ballVector;
 public:
 	Ball(float x,float y,Math::Vector2 vector,float speed,float radius);
 	Math::Vector2 getVect();
